e6497a788d5792b951c9d4bfc6560772ab2bde96
りんめ <rinmesk@yahoo.com>
2024-05-29 01:59:48 +0700

    feat: Removed error handler (for now)

--- src/router/user.ts
+++ src/router/user.ts
@@ -3,14 +3,14 @@ import { getData, getAvatar } from "../event/discord/getUser";
 
 export const users = new Elysia().group("/user", (app) =>
   app
-    .get("/:id", ({ params: { id }, error }) => getData(id), {
+    .get("/:id", ({ params: { id } }) => getData(id), {
       detail: {
         tags: ["User"],
       },
     })
     .get(
       "/avatar/:id",
-      async ({ params: { id }, redirect, error }) =>
+      async ({ params: { id }, redirect }) =>
         redirect(await getAvatar(id)),
       {
         detail: {
