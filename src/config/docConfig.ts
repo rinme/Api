2db025bea3e63cd7c01e483276482711bdf9ac53
りんめ <rinmesk@yahoo.com>
2024-05-28 21:36:38 +0700

    added: docConfig for Swagger

---
+++ src/config/docConfig.ts
@@ -0,0 +1,16 @@
+export const apiConfig = {
+  swagger: {
+    path: "/apis",
+    documentation: {
+      info: {
+        title: "something Documentation",
+        version: "1.0.69",
+      },
+      tags: [
+        { name: "User", description: "Discord User Information endpoints" },
+        { name: "Swagger", description: "Swagger Documentation" },
+        { name: "Etc", description: "Etc endpoints" },
+      ],
+    },
+  },
+};
