32d2e3a295b69596078c8c43a1a886d41f13461b
りんめ <rinmesk@yahoo.com>
2024-05-28 21:36:04 +0700

    added: index.ts, discordjs

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { Client, GatewayIntentBits } from "discord.js";
+import { app } from "./router/route";
+require("dotenv").config();
+
+export const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
+});
+
+client.once("ready", () => {
+  console.log("🦊 Discord is ready!");
+});
+
+client.login(process.env.DTOKEN);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+);
