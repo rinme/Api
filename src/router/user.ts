a8398af454938346b91e15dd11b50961bddb6c00
りんめ <rinmesk@yahoo.com>
2024-05-28 23:26:16 +0700

    feat: format

--- src/router/user.ts
+++ src/router/user.ts
@@ -3,14 +3,15 @@ import { getData, getAvatar } from "../event/discord/getUser";
 
 export const users = new Elysia().group("/user", (app) =>
   app
-    .get("/:id", ({ params: { id } }) => getData(id), {
+    .get("/:id", ({ params: { id }, error }) => getData(id), {
       detail: {
         tags: ["User"],
       },
     })
     .get(
       "/avatar/:id",
-      async ({ params: { id }, redirect }) => redirect(await getAvatar(id)),
+      async ({ params: { id }, redirect, error }) =>
+        redirect(await getAvatar(id)),
       {
         detail: {
           tags: ["User"],
