40bf4492c014ebb876907f09f646092df796bade
りんめ <rinmesk@yahoo.com>
2024-07-12 20:30:40 +0700

    feat: updated to json way (I mean i was very dumb)

--- src/event/discord/getUser.ts
+++ src/event/discord/getUser.ts
@@ -1,11 +1,26 @@
 import { client } from "../..";
 import { NotFoundError } from "elysia";
 
+type User = {
+  id: string;
+  username: string;
+  discriminator: string;
+  avatar: string;
+};
+
 // get data from id
 export const getData = async (id: string) => {
   try {
     const user = await client.users.fetch(id);
-    return `User's ID is: ${id}, Username: ${user.username}, Discriminator: ${user.discriminator}, Avatar: ${user.displayAvatarURL({ extension: "png" })}`;
+    let users: User[] = [
+      {
+        id: id,
+        username: user.username,
+        discriminator: user.discriminator,
+        avatar: user.displayAvatarURL({ extension: "png" }),
+      },
+    ];
+    return users;
   } catch (error) {
     return new NotFoundError();
   }
